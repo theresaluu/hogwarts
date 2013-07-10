9bbc13b1c701d59180dcd58e8d190f2e5f04e0be
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-07-10 00:06:16 -0400

    added new page

--- db/migrate/20130710032732_create_students_table.rb
+++ db/migrate/20130710032732_create_students_table.rb
@@ -2,8 +2,6 @@ class CreateStudentsTable < ActiveRecord::Migration
   def up
     create_table :students do |t|
       t.name :string
-      t.age :integer
-      t.pet_name :string
     end
   end
 
