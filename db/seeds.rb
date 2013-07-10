12b59303ad440ea979c7cc4ad52ed56c259912bd
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-07-10 00:06:41 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+House.delete_all
+Student.delete_all
+
+# http://harrypotter.wikia.com/wiki/Hogwarts_Houses
+House.create(name: "Gryffendor")
+House.create(name: "Slytherin")
+House.create(name: "Hufflepuff")
+House.create(name: "Ravenclaw")
+
+puts "Houses created"
+
+# http://harrypotter.wikia.com/wiki/Category:Hogwarts_students
+Student.create(name: "Harry Potter")
+Student.create(name: "Hermione")
+Student.create(name: "Ron")
+Student.create(name: "Draco")
+
+puts "Students created."
